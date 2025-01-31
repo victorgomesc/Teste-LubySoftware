039ec8c4f0f3c1a8be0331a6b0cacb2010a96ef0
Victor Gomes <victorgomesdacosta18@gmail.com>
2025-01-31 18:13:50 -0300

    feat: I implement questao1.1

--- Logica-de-programacao/questao1.1/Program.cs
+++ Logica-de-programacao/questao1.1/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite um número para calcular o fatorial: ");
+        if (int.TryParse(Console.ReadLine(), out int numero))
+        {
+            try
+            {
+                long resultado = CalcularFatorial(numero);
+                Console.WriteLine($"O fatorial de {numero} é {resultado}");
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine($"Erro: {e.Message}");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Entrada inválida! Digite um número inteiro válido.");
+        }
+    }
+    static long CalcularFatorial(int numero)
+    {
+        if (numero < 0)
+            throw new ArgumentException("O número não pode ser negativo.");
+
+        long resultado = 1;
+        for (int i = 2; i <= numero; i++)
+        {
+            resultado *= i;
+        }
+        return resultado;
+    }
+}
+
