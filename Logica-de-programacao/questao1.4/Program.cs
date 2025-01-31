b17cd30e46e6f647695f4277538274a4e4911dc7
Victor Gomes <victorgomesdacosta18@gmail.com>
2025-01-31 18:36:35 -0300

    feat: i implement questao1.4

--- Logica-de-programacao/questao1.4/Program.cs
+++ Logica-de-programacao/questao1.4/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite uma frase ou palavra: ");
+        string entrada = Console.ReadLine();
+
+        if (string.IsNullOrWhiteSpace(entrada))
+        {
+            Console.WriteLine("Erro: A entrada não pode ser vazia.");
+            return;
+        }
+
+        int totalVogais = CalcularVogais(entrada);
+        Console.WriteLine($"Total de vogais na string: {totalVogais}");
+    }
+
+    static int CalcularVogais(string texto)
+    {
+        int contador = 0;
+        string vogais = "aeiouAEIOU";
+
+        foreach (char c in texto)
+        {
+            if (vogais.Contains(c))
+            {
+                contador++;
+            }
+        }
+
+        return contador;
+    }
+}
+
