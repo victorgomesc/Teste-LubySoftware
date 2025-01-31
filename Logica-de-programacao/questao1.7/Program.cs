b19ec2d74f635cf8a6dee1450d469e45afdf1389
Victor Gomes <victorgomesdacosta18@gmail.com>
2025-01-31 19:24:19 -0300

    feat: implement questao1.7

--- Logica-de-programacao/questao1.7/Program.cs
+++ Logica-de-programacao/questao1.7/Program.cs
@@ -1,2 +1,52 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite o tamanho do vetor: ");
+        int tamanho = int.Parse(Console.ReadLine());
+
+        int[] vetor = new int[tamanho];
+
+        for (int i = 0; i < tamanho; i++)
+        {
+            Console.Write($"Digite o valor para a posição {i}: ");
+            vetor[i] = int.Parse(Console.ReadLine());
+        }
+
+        int[] pares = ObterElementosPares(vetor);
+
+        Console.WriteLine("Elementos pares:");
+        foreach (int num in pares)
+        {
+            Console.WriteLine(num);
+        }
+    }
+
+    static int[] ObterElementosPares(int[] vetor)
+    {
+        int count = 0;
+        for (int i = 0; i < vetor.Length; i++)
+        {
+            if (vetor[i] % 2 == 0)
+            {
+                count++;
+            }
+        }
+
+        int[] pares = new int[count];
+
+        int index = 0;
+        for (int i = 0; i < vetor.Length; i++)
+        {
+            if (vetor[i] % 2 == 0)
+            {
+                pares[index] = vetor[i];
+                index++;
+            }
+        }
+
+        return pares;
+    }
+}
