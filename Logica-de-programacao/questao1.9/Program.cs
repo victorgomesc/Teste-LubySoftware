a3615c9b5ae49ca69741216575521ba68a86044d
Victor Gomes <victorgomesdacosta18@gmail.com>
2025-02-01 15:00:59 -0300

    feat: implement questao1.9

--- Logica-de-programacao/questao1.9/Program.cs
+++ Logica-de-programacao/questao1.9/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Digite os números separados por vírgula:");
+        string entrada = Console.ReadLine();
+        
+        int[][] resultado = TransformarEmMatriz(entrada);
+        
+        foreach (var subArray in resultado)
+        {
+            Console.WriteLine("[ " + string.Join(", ", subArray) + " ]");
+        }
+    }
+
+    static int[][] TransformarEmMatriz(string numeros)
+    {
+        string[] partes = numeros.Split(',');
+        int[] valores = new int[partes.Length];
+        for (int i = 0; i < partes.Length; i++)
+        {
+            valores[i] = int.Parse(partes[i]);
+        }
+        
+        int quantidadeSubArrays = (valores.Length + 1) / 2;
+        int[][] matriz = new int[quantidadeSubArrays][];
+        
+        for (int i = 0; i < quantidadeSubArrays; i++)
+        {
+            int tamanho = (i * 2 + 1 < valores.Length) ? 2 : 1;
+            matriz[i] = new int[tamanho];
+            for (int j = 0; j < tamanho; j++)
+            {
+                matriz[i][j] = valores[i * 2 + j];
+            }
+        }
+        
+        return matriz;
+    }
+}
+
